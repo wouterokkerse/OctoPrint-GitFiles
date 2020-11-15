8835c56f9de200825cf2baaa8f14d51407fc0c8a
Robert McKenzie <rmckenzi@rpmdp.com>
2020-11-15 22:52:09 +0000

    First pass to add custom branch

--- octoprint_gitfiles/__init__.py
+++ octoprint_gitfiles/__init__.py
@@ -42,6 +42,7 @@ class GitfilesPlugin(octoprint.plugin.SettingsPlugin,
 			uploads = self._settings.global_get_basefolder("uploads")
 			path =    self._settings.get(["path"])
 			url =     self._settings.get(["url"])
+			mybranch =  self._settings.get(["mybranch"])
 			verb =    "{arg1}".format(**data)
 			
 			if path == "" or path == "uploads":
@@ -63,8 +64,8 @@ class GitfilesPlugin(octoprint.plugin.SettingsPlugin,
 
 			# This one runs regardless of whether or not it's been previously initialized
 			try:
-				self._logger.info("-- git {} origin master ---------------------------------------------------".format(verb))
-				output =  call(["git", verb, "origin", "master"], cwd=gitfilesFolder)
+				self._logger.info("-- git {} origin" + mybranch + " ---------------------------------------------------".format(verb))
+				output =  call(["git", verb, "origin", mybranch], cwd=gitfilesFolder)
 				self._logger.info("git returned: " + str(output))
 				self._logger.info("-- (end of git {}) --------------------------------------------------------".format(verb))
 			except OSError as e:
@@ -89,7 +90,7 @@ class GitfilesPlugin(octoprint.plugin.SettingsPlugin,
 			self._logger.info("`git init` failed")
 			return
 		try:
-			self._logger.info("Setting up the remote origin for master...")
+			self._logger.info("Setting up the remote origin for " + mybranch + "...")
 			output =  call(["git", "remote", "add", "origin", url], cwd=gitfilesFolder)
 			self._logger.info(output)
 		except OSError as e:
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "gitfiles"
+plugin_identifier = "gitfiles-m1xzg"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_gitfiles"
+plugin_package = "octoprint_gitfiles-m1xzg"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "GitFiles"
+plugin_name = "GitFiles-m1xzg"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.1.5"
@@ -27,7 +27,7 @@ plugin_author = "OutsourcedGuru"
 plugin_author_email = "support@outsourced.guru"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/OutsourcedGuru/OctoPrint-GitFiles"
+plugin_url = "https://github.com/M1XZG/OctoPrint-GitFiles"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "MIT"
